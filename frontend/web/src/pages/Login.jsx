5308a569f5c754551b56157022accd9d1063a46f
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-10-15 23:15:08 -0400

    Finish basic login

--- frontend/web/src/pages/Login.jsx
+++ frontend/web/src/pages/Login.jsx
@@ -5,11 +5,23 @@ import "../styles.scss";
 function Login() {
 	return (
 		<div className="login">
-			<form>
-				<input className="login-input" placeholder="Email address" />
-				<input className="login-input" placeholder="Password" />
-				<button type="submit">Submit</button>
-			</form>
+			<div className="hero">
+				<h1>UCFEats</h1>
+				<span>Keeping UCF Students Healthy</span>
+			</div>
+			<div className="form">
+				<div className="login-box">
+					<h1>Sign In</h1>
+					<form>
+						<input className="login-input" placeholder="Email address" />
+						<input className="login-input" placeholder="Password" />
+						<button type="submit">Submit</button>
+						<span>Forgot password?</span>
+						<span>Need an account?</span>
+					</form>
+					<div></div>
+				</div>
+			</div>
 		</div>
 	);
 }
