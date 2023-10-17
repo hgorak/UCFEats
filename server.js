48d8ac6b8d021cb68288b9005bbe5c4acc319946
Brendan-Smith529 <brendansmith5292002@gmail.com>
2023-10-17 18:59:39 -0400

    Changed file structure to accomodate for Heroku

---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const groceryRoutes = require('./backend/routes/groceries');
+const userRoutes = require('./backend/routes/user');
+
+// 
+const PORT = process.env.PORT || 4000;
+
+// Creates an express app
+const app = express();
+
+// set PORT
+app.set('port', (process.env.PORT || 4000));
+
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes
+app.use('/api/groceries', groceryRoutes);
+app.use('/api/user', userRoutes);
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for requests
+    app.listen(PORT || 4000, () => {
+      console.log("Connected to db & listening on port", process.env.PORT);
+    });
+  })
+
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production')
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
