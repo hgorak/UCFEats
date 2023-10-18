12bc7a977de6564728e2b30e1bcc06c7810816e6
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-10-18 17:52:37 -0400

    Add hero

--- frontend/web/src/pages/Landing.jsx
+++ frontend/web/src/pages/Landing.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
 import Navbar from "../components/Navbar.jsx";
+import Phone from "../images/phone.jpg";
 import "../styles.scss";
 
 function Landing() {
-	return <Navbar />;
+	return (
+		<div className="landing">
+			<Navbar />
+			<div className="hero">
+				<div className="container-1">
+					<div className="description">
+						<h1>Find good eats</h1>
+						<span>with UCFEats</span>
+					</div>
+				</div>
+				<div className="container-2">
+					<img src={Phone} alt="phone" />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Landing;
