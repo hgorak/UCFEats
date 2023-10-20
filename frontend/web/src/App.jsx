6de731dcd093ed4bae97a5a439a372469f14cd5b
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-10-20 14:49:12 -0400

    Add routing and error page

--- frontend/web/src/App.jsx
+++ frontend/web/src/App.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
 import Landing from "./pages/Landing.jsx";
+import Error from "./pages/Error.jsx";
 
 import "./styles.scss";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Landing />,
+		errorElement: <Error />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+]);
 
 function App() {
 	return (
 		<div className="app">
 			<div className="container">
-				<Landing />
+				<RouterProvider router={router} />
 			</div>
 		</div>
 	);
