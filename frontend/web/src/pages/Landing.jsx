c4e62de38588248e084264f1e203625fe3fcf44c
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-10-22 22:27:05 -0400

    Add members to team

--- frontend/web/src/pages/Landing.jsx
+++ frontend/web/src/pages/Landing.jsx
@@ -6,6 +6,8 @@ import { IconContext } from "react-icons";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 import Phone from "../images/phone.png";
+
+import darrenHead from "../images/darren.jpg";
 import "../styles.scss";
 
 function Landing() {
@@ -46,6 +48,100 @@ function Landing() {
 						"This is the best app I've ever <s>made</s> used in my entire life!"
 					</span>
 					<span className="name">- Darren B.</span>
+				</div>
+			</div>
+			<div className="about">
+				<div className="container">
+					<h1>Meet the team.</h1>
+					<span></span>
+					<div className="row">
+						<div className="member">
+							<div className="head">
+								<img src={darrenHead} alt="darren headshot" />
+							</div>
+							<div className="desc">
+								<h2>
+									Darren<br></br>Bansil
+								</h2>
+								<span>Web Developer</span>
+								<div className="links">Github Linkedin</div>
+							</div>
+						</div>
+						<div className="member">
+							<div className="head">
+								<img src={darrenHead} alt="darren headshot" />
+							</div>
+							<div className="desc">
+								<h2>
+									Akil<br></br>Mohideen
+								</h2>
+								<span>Web Developer</span>
+								<div className="links">Github Linkedin</div>
+							</div>
+						</div>
+						<div className="member">
+							<div className="head">
+								<img src={darrenHead} alt="darren headshot" />
+							</div>
+							<div className="desc">
+								<h2>
+									Jose<br></br>Cuyugan
+								</h2>
+								<span>Mobile Developer</span>
+								<div className="links">Github Linkedin</div>
+							</div>
+						</div>
+						<div className="member">
+							<div className="head">
+								<img src={darrenHead} alt="darren headshot" />
+							</div>
+							<div className="desc">
+								<h2>
+									Jacob<br></br>Dioso
+								</h2>
+								<span>Mobile Developer</span>
+								<div className="links">Github Linkedin</div>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="member">
+							<div className="head">
+								<img src={darrenHead} alt="darren headshot" />
+							</div>
+							<div className="desc">
+								<h2>
+									Hailey<br></br>Gorak
+								</h2>
+								<span>API Developer</span>
+								<div className="links">Github Linkedin</div>
+							</div>
+						</div>
+						<div className="member">
+							<div className="head">
+								<img src={darrenHead} alt="darren headshot" />
+							</div>
+							<div className="desc">
+								<h2>
+									Brendan<br></br>Smith
+								</h2>
+								<span>Mobile Developer</span>
+								<div className="links">Github Linkedin</div>
+							</div>
+						</div>
+						<div className="member">
+							<div className="head">
+								<img src={darrenHead} alt="darren headshot" />
+							</div>
+							<div className="desc">
+								<h2>
+									Alex<br></br>Cruz
+								</h2>
+								<span>Database Developer</span>
+								<div className="links">Github Linkedin</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<Footer />
