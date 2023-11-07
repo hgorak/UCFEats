55df1fa8146efed9ee9dc8e07fa8e9eedc44c517
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-11-07 10:21:23 -0500

    Fix css specificity after importing bootstrap

--- frontend/web/src/App.jsx
+++ frontend/web/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
+import Forgot from "./pages/Forgot.jsx";
 import Landing from "./pages/Landing.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Home from "./pages/dash-pages/Home.jsx";
@@ -42,6 +43,11 @@ function App() {
 						<Route path="/" element={<Landing />} errorElement={<Error />} />
 						<Route path="/login" element={<Login />} errorElement={<Error />} />
 						<Route path="/register" element={<Register />} errorElement={<Error />} />
+						<Route
+							path="/forgotpassword"
+							element={<Forgot />}
+							errorElement={<Error />}
+						/>
 						<Route
 							path="/dashboard"
 							element={currentUser ? <Dashboard /> : <Navigate to="/" />}
