2fe9128e74aa1acc92ee74700aa6e055c5fcecec
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-11-13 18:29:18 -0500

    Reset password and show success

--- frontend/web/src/pages/Reset.jsx
+++ frontend/web/src/pages/Reset.jsx
@@ -6,7 +6,30 @@ import "../styles.scss";
 
 function Reset() {
 	let { resetToken } = useParams();
+	const [error, setError] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const [reset, setReset] = useState(false);
 
+	const resetPass = async (event) => {
+		setError(null);
+		event.preventDefault();
+
+		const response = await fetch(API_URL + "/api/user/reset/" + resetToken, {
+			method: "PATCH",
+			body: JSON.stringify({ password: newPassword }),
+			headers: { "Content-Type": "application/JSON" },
+		});
+
+		const json = await response.json();
+
+		if (!response.ok) {
+			setError(json.message);
+		}
+
+		if (response.ok) {
+			setReset(true);
+		}
+	};
 	return (
 		<div className="login">
 			<div className="navbar-container navbar-container-bg">
@@ -19,29 +42,42 @@ function Reset() {
 				</div>
 			</div>
 			<div className="container">
-				<div className="forgot-box">
-					<h1>Change your Password</h1>
-					<span>
-						Make this one memorable. <br></br>Or super complicated. Up to you.
-					</span>
-					<span>{resetToken}</span>
-					<form>
-						<input
-							required
-							type="password"
-							className="login-input"
-							placeholder="New Password"
-						/>
-						<input
-							required
-							type="email"
-							className="login-input"
-							placeholder="Confirm New Password"
-						/>
-						<button type="submit">Change Password</button>
-						{/* {error && <div className="error">{error}</div>} */}
-					</form>
-				</div>
+				{sent ? (
+					<div className="forgot-box forgot-box-sent">
+						<h1>Password Successfully Reset</h1>
+						<span>
+							Enjoy your new password! <Link to="/login">Log in</Link>
+						</span>
+					</div>
+				) : (
+					<div className="forgot-box">
+						<h1>Change your Password</h1>
+						<span>
+							Make this one memorable. <br></br>Or super complicated. Up to you.
+						</span>
+						<form>
+							<input
+								required
+								type="password"
+								className="login-input"
+								placeholder="New Password"
+							/>
+							<input
+								required
+								type="password"
+								className="login-input"
+								placeholder="Confirm New Password"
+								onChange={(e) => {
+									setNewPassword(e.target.value);
+								}}
+							/>
+							<button type="submit" onClick={resetPass}>
+								Change Password
+							</button>
+							{error && <div className="error">{error}</div>}
+						</form>
+					</div>
+				)}
 			</div>
 		</div>
 	);
