da4590c9545f381cc2b5dd6f3f0293ed852e4703
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-11-14 21:43:53 -0500

    Refactor verification

--- frontend/web/src/components/Verification.jsx
+++ frontend/web/src/components/Verification.jsx
@@ -52,35 +52,25 @@ function Verification() {
 		);
 	}
 
-	return (
-		<div className="login">
-			<div className="navbar-container navbar-container-bg">
-				<div className="navbar">
-					<div className="title">
-						<Link to="/">
-							<h1>UCFEats</h1>
-						</Link>
-					</div>
+	if (verified) {
+		return (
+			<div className="forgot-box forgot-box-sent">
+				<div className="auth-header">
+					<h1>Email Verified</h1>
+					<span>
+						Let's use your new account! <Link to="/login">Log in</Link>
+					</span>
 				</div>
 			</div>
-			<div className="container">
-				{verified ? (
-					<div className="forgot-box forgot-box-sent">
-						<h1>Email Verified</h1>
-						<span>
-							Let's use your new account! <br></br>
-							<Link to="/login">Log in</Link>
-						</span>
-					</div>
-				) : (
-					<div className="forgot-box forgot-box-sent">
-						<h1>Verification Unsuccessful</h1>
-						<span>We just sent you another verification email.</span>
-					</div>
-				)}
+		);
+	} else {
+		return (
+			<div className="forgot-box forgot-box-sent">
+				<h1>Verification Unsuccessful</h1>
+				<span>We just sent you another verification email.</span>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default Verification;
