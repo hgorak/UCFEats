a59af1d73d23d32b60b87664737282a35340cb3d
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-11-29 08:47:07 -0500

    Add timeout after verification and redirect to dashboard

---
+++ frontend/web/src/components/VerifiedBox.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { API_URL } from "../../api.js";
+
+import "../styles.scss";
+
+function VerifiedBox({ children }) {
+	const [show, setShow] = useState(true);
+
+	// On componentDidMount set the timer
+	useEffect(() => {
+		const timeId = setTimeout(() => {
+			// After 3 seconds set the show value to false
+			setShow(false);
+		}, 3000);
+
+		return () => {
+			clearTimeout(timeId);
+		};
+	}, []);
+
+	// If show is false the component will return null and stop here
+	if (!show) {
+		return null;
+	}
+
+	return <div className="forgot-box forgot-box-sent">{children}</div>;
+}
+
+export default VerifiedBox;
