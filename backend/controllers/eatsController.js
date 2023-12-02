f441e2d89c43e155ad77e5a14d3c2509fb9df7b6
Brendan-Smith529 <brendansmith5292002@gmail.com>
2023-12-02 18:10:10 -0500

    Update day eats return; Server was switched to EST

--- backend/controllers/eatsController.js
+++ backend/controllers/eatsController.js
@@ -179,12 +179,12 @@ const deleteEat = async (req, res) => {
 /* Eats by Time Period */
 const dayEats = async (req, res) => {
   let start = new Date();
-  start.setHours(5, 0, 0, 0);
-  start.setMinutes(start.getMinutes()); // Adjust for UTC-5
+  start.setHours(0, 0, 0, 0);
+  start.setMinutes(start.getMinutes());
 
   let end = new Date();
   end.setHours(23, 59, 59, 999);
-  end.setMinutes(end.getMinutes()); // Adjust for UTC-5
+  end.setMinutes(end.getMinutes());
   
   const eats = await EatsList.find({user_id: req.user._id, updatedAt: {$gte: start, $lt: end}}).sort({updatedAt: 1});
 
